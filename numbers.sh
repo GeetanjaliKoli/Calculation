8625c80375f365277a841f458d18f4ab28b7db7d
Geetanjali Koli <k0ligeet@hotmail.com>
2022-05-08 15:10:34 +0530

    Added: numbers file

---
+++ numbers.sh
@@ -0,0 +1,21 @@
+# calci  with numbers
+
+num1=10
+num2=20
+
+#addition
+
+sum=$((num1 + num2))
+echo $sum
+
+#Substraction
+
+sub=$((num1 - num2))
+echo $sub
+
+#multiplication
+
+mul=$((num1 * num2))
+echo $mul
+
+
